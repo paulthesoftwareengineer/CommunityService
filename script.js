8e8888e07e72451064ba2cc99358d0dc91bb3996
Paul Turner <baldheader@live.com>
2023-07-29 12:54:32 -0700

    index

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-// Sample data for community service projects
-const projects = [
-    { name: 'Project 1', category: 'education', description: 'Description of Project 1' },
-    { name: 'Project 2', category: 'environment', description: 'Description of Project 2' },
-    { name: 'Project 3', category: 'health', description: 'Description of Project 3' },
-    // Add more projects as needed
-  ];
-  
-  // Function to display projects based on selected category
-  function displayProjects(category) {
-    const projectListElement = document.getElementById('projectList');
-    let html = '';
-  
-    projects.forEach(project => {
-      if (category === 'all' || category === project.category) {
-        html += `
-          <div class="card my-3">
-            <div class="card-body">
-              <h5 class="card-title">${project.name}</h5>
-              <p class="card-text">${project.description}</p>
-              <p class="card-text"><strong>Category:</strong> ${project.category}</p>
-            </div>
+// Sample data for racing teams
+const racingTeams = [
+  { name: 'Team A', category: 'formula1', description: 'Description of Team A' },
+  { name: 'Team B', category: 'nascar', description: 'Description of Team B' },
+  { name: 'Team C', category: 'motogp', description: 'Description of Team C' },
+  // Add more racing teams as needed
+];
+
+// Function to display racing teams based on selected category
+function displayRacingTeams(category) {
+  const teamListElement = document.getElementById('teamList');
+  let html = '';
+
+  racingTeams.forEach(team => {
+    if (category === 'all' || category === team.category) {
+      html += `
+        <div class="card my-3">
+          <div class="card-body">
+            <h5 class="card-title">${team.name}</h5>
+            <p class="card-text">${team.description}</p>
+            <p class="card-text"><strong>Category:</strong> ${team.category}</p>
           </div>
-        `;
-      }
-    });
-  
-    projectListElement.innerHTML = html;
-  }
-  
-  // Add event listener to the category filter
-  document.getElementById('categorySelect').addEventListener('change', function() {
-    const selectedCategory = this.value;
-    displayProjects(selectedCategory);
+        </div>
+      `;
+    }
   });
-  
-  // Display all projects initially
-  displayProjects('all');
-  
+
+  teamListElement.innerHTML = html;
+}
+
+// Add event listener to the race category filter
+document.getElementById('categorySelect').addEventListener('change', function() {
+  const selectedCategory = this.value;
+  displayRacingTeams(selectedCategory);
+});
+
+// Display all racing teams initially
+displayRacingTeams('all');
